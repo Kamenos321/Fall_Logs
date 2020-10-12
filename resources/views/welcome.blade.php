a8e69b2abe7a74b6f6a0651f7fb58a1fd8aee6fd
daichi.kameya <kamenos321@gmail.com>
2020-10-12 13:27:16 +0000

    Procfile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,17 @@
 
 
     @if (Auth::check())
-        <div class='container'>
-            <img src='{{ asset('storage/logo.jpg') }}' class='row col-sm-10 mx-auto rounded-pill'>
-            <div class='row col-12 mt-3 mx-auto'>
-                {{-- show作成ページへのリンク --}}
-                {!! link_to_route('shows.create', 'show作成', [], ['class' => 'main_button btn btn-primary rounded-pill row col-sm-4 mx-auto']) !!}
-                {{-- show一覧ページへのリンク --}}
-                {!! link_to_route('shows.index', 'show一覧', [], ['class' => 'main_button btn btn-success rounded-pill row col-sm-4 mx-auto']) !!}
-                {{-- statsページへのリンク --}}
-                {!! link_to_route('users.stats', 'スタッツ', [], ['class' => 'main_button btn btn-danger rounded-pill row col-sm-4 mx-auto']) !!}
-            </div>
+
+        <img src='{{ asset('storage/logo.jpg') }}' class='row col-sm-12 mx-auto rounded-pill'>
+        <div class='row col-12 mt-3 mx-auto'>
+            {{-- show作成ページへのリンク --}}
+            {!! link_to_route('shows.create', 'show作成', [], ['class' => 'main_button btn btn-primary rounded-pill row col-sm-4 mx-auto']) !!}
+            {{-- show一覧ページへのリンク --}}
+            {!! link_to_route('shows.index', 'show一覧', [], ['class' => 'main_button btn btn-success rounded-pill row col-sm-4 mx-auto']) !!}
+            {{-- statsページへのリンク --}}
+            {!! link_to_route('users.stats', 'スタッツ', [], ['class' => 'main_button btn btn-danger rounded-pill row col-sm-4 mx-auto']) !!}
         </div>
+
     @else
         <div class="center jumbotron">
             <div class="text-center">
