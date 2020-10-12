859eb167214055cf14dc90fb9d0307e5e25f2741
daichi.kameya <kamenos321@gmail.com>
2020-10-12 17:14:29 +0000

    gitignore

--- resources/views/shows/index.blade.php
+++ resources/views/shows/index.blade.php
@@ -9,7 +9,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     </head>
 
-    <body style='background-image: url("../storage/background_index.jpg");'>
+    <body style='background-image: url("storage/background_index.jpg");'>
 
       {{-- ナビゲーションバー --}}
       @include('commons.navbar')
